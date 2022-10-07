cbdae00ec2df3e9e95ed257abe489327aa150292
FarizaNasreen <fariza.nasreen33@gmail.com>
2022-10-08 00:10:54 +0600

    Module 30 practice

---
+++ let-const.js
@@ -0,0 +1,47 @@
+// console.log('Okay');
+// const const1 = (first, second, third) => first * second * third;
+// const result = const1(10 , 20, 30 );
+// console.log(result);
+//2
+// const newMultiline = `I am a web-developer
+// I like to code
+// I love to eat biriyani`;
+
+// console.log(newMultiline);
+// 3
+// const const1 = (first, second=2) => first * second ;
+// const result1 = const1(1);
+// console.log(result1);
+//4
+// function ArrayFunction(){
+// let newArray = [];
+// const const2Array =['Badhon', 'Nubaira', 'Nabila', 'Preeti', 'Luna', 'Chhoa'];
+// for (let i=0; i<const2Array.length; i++ ) {
+// if ([const2Array[i].length%2]==0)
+//     //console.log('Even');
+//     newArray.push(const2Array[i]);
+// }
+// return newArray;
+// }
+// let result= ArrayFunction();
+// console.log(result);
+
+ //5-3a
+//  const const3 = num => num * num;
+//  result5 =const3(5);
+//  console.log(result5);
+//  result6=const3(2);
+//  result7 = result5 + result6;
+//  console.log (result7);
+//  const avg = result7/2;
+//  console.log(avg);
+ //6
+ 
+// const1=[1, 2, 3];
+// const2=[4, 5, 6];
+
+// const merged= [...const1, ...const2];
+// const maxi= Math.max(...merged);
+// console.log(maxi);
+
+//7
